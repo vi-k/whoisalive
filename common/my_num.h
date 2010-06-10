09e2f4dc89933b575d96d01e2f0c704bd70934e0
victor dunaev (victor dunaev <victor dunaev (victor.dunaev@gmail.com)>
2010-06-10 13:12:02 +1100

    ??????? my::num::num_to_string

--- common/my_num.h
+++ common/my_num.h
@@ -1,10 +1,208 @@
-﻿#ifndef MY_NUM_H
+﻿/*
+	Быстрые функции преобразования чисел в строки и из строки
+	
+	В строку (signed_to, unsigned_to)- своя функция (первый придуманный
+		алгоритм оказался быстрее itoa и Boost.Spirit.Karma (но после
+		всех наворотов стал таким же как Karma).
+
+	Из строки (to_signed, to_unsigned, to_real)- использую
+		Boost.Spirit2 (ужасно медленно компилируется, поэтому шаблоны
+		вынес в *.cpp (и чтоб они компилировались, а не игнорировались,
+		включил функцию my_num_dummy, которая задействует все возможные
+		варианты).
+*/
+
+#ifndef MY_NUM_H
 #define MY_NUM_H
 
 #include <cstddef> /* std::size_t */
 #include <string>
 
 namespace my { namespace num {
+
+
+#define NUM_TO_STR_BUF_SIZE 64
+
+/*
+	signed_to
+
+	Алгоритм преобразования целого числа со знаком в строку.
+	Ради производительности имеет ограничение - используется
+	временный буфер размером NUM_TO_STR_BUF_SIZE байт
+	(128 бит требует 39 байт).
+	
+	Напрямую использовать не рекомендуется, т.к. шаблон принимает
+	любые типы и результат такого действия не предсказуем.
+*/
+template<class T, class Char>
+std::size_t signed_to(T n, Char *str, std::size_t size,
+	std::size_t decimals = 0)
+{
+	static const Char sym[]
+		= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	/* Результат сохраняем во временный буфер в обратном порядке */
+	Char buf[NUM_TO_STR_BUF_SIZE];
+	Char *buf_ptr = buf;
+	bool neg;
+
+	/* Сохраняем знак. Операции проводим только с отрицательными
+		числами. Логичнее было бы проводить с положительными,
+		но не все отрицательные числа можно перевести
+		в положительные - например, (char)-128 */
+	if ( (neg = n < 0) == false)
+		n = -n;
+
+	do
+	{
+		T nn = n / 10;
+		*buf_ptr++ = sym[ (nn<<3) + (nn<<1) - n ];
+		n = nn;
+	} while (n);
+
+	/* На данный момент в буфере хранится число в обратном
+		порядке, без знака и без завершающего нуля */
+
+	/* Заполняем выходную строку. Отдельно, если не входим
+		в ограничение размера (###), отдельно - если входим */
+	Char *str_ptr = str;
+	std::size_t buf_size = buf_ptr - buf;
+	std::size_t zero_count = (decimals > buf_size ? decimals - buf_size : 0);
+
+	if (buf_size + zero_count + neg >= size)
+	{
+		if (size != 0)
+		{
+			Char *str_last = str + size - 1;
+			while (str_ptr != str_last)
+   				*str_ptr++ = '#';
+
+   			*str_ptr = 0;
+		}	
+   	}
+   	else
+   	{
+		if (neg)
+			*str_ptr++ = '-';
+
+		while (zero_count)
+			zero_count--, *str_ptr++ = '0';
+
+		while (buf_ptr != buf)
+   			*str_ptr++ = *--buf_ptr;
+
+		*str_ptr = 0;   	
+   	}
+
+	return str_ptr - str;
+}
+
+/*
+	unsigned_to
+
+	Алгоритм преобразования целого числа без знака в строку.
+	Ради производительности имеет ограничение - используется
+	временный буфер размером NUM_TO_STR_BUF_SIZE байт
+	(128 бит требует 39 байт).
+	
+	Напрямую использовать не рекомендуется, т.к. шаблон принимает
+	любые типы и результат такого действия не предсказуем.
+*/
+
+template<class T, class Char>
+std::size_t unsigned_to(T n, Char *str, std::size_t size,
+	std::size_t decimals = 0)
+{
+	static const Char sym[]
+		= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	/* Результат сохраняем во временный буфер в обратном порядке */
+	Char buf[NUM_TO_STR_BUF_SIZE];
+	Char *buf_ptr = buf;
+
+	do
+	{
+		T nn = n / 10;
+		*buf_ptr++ = sym[ n - (nn<<3) - (nn<<1) ];
+		n = nn;
+	} while (n);
+
+	/* На данный момент в буфере хранится число в обратном
+		порядке, без завершающего нуля */
+
+	/* Заполняем выходную строку. Отдельно, если не входим
+		в ограничение размера (###), отдельно - если входим */
+	Char *str_ptr = str;
+	std::size_t buf_size = buf_ptr - buf;
+	std::size_t zero_count = (decimals > buf_size ? decimals - buf_size : 0);
+
+	if (buf_size + zero_count >= size)
+	{
+		if (size != 0)
+		{
+			Char *str_last = str + size - 1;
+			while (str_ptr != str_last)
+   				*str_ptr++ = '#';
+
+   			*str_ptr = 0;
+		}	
+   	}
+   	else
+   	{
+		while (zero_count)
+			zero_count--, *str_ptr++ = '0';
+
+		while (buf_ptr != buf)
+   			*str_ptr++ = *--buf_ptr;
+
+		*str_ptr = 0;   	
+   	}
+
+	return str_ptr - str;
+}
+
+template<class T, class Char>
+inline std::basic_string<Char> signed_to(T n, std::size_t decimals = 0)
+{
+	Char buf[NUM_TO_STR_BUF_SIZE];
+	std::size_t sz
+		= signed_to<T,Char>(n, buf, sizeof(buf), decimals);
+	return std::basic_string<Char>(buf);
+}
+
+template<class T, class Char>
+inline std::basic_string<Char> unsigned_to(T n, std::size_t decimals = 0)
+{
+	Char buf[NUM_TO_STR_BUF_SIZE];
+	std::size_t sz
+		= unsigned_to<T,Char>(n, buf, sizeof(buf), decimals);
+	return std::basic_string<Char>(buf);
+}
+
+#define DEF_NUM_TO_FUNCS(S,N,T)\
+template<class Char>\
+inline std::size_t N##_to(T n, Char *str, std::size_t size,\
+	std::size_t decimals = 0)\
+	{ return S##_to<T,Char>(n, str, size, decimals); }\
+template<class Char>\
+inline std::basic_string<Char> N##_to(T n, std::size_t decimals = 0)\
+	{ return S##_to<T,Char>(n, decimals); }\
+inline std::string N##_to_string(T n, std::size_t decimals = 0)\
+	{ return S##_to<T,char>(n, decimals); }\
+inline std::wstring N##_to_wstring(T n, std::size_t decimals = 0)\
+	{ return S##_to<T,wchar_t>(n, decimals); }
+
+DEF_NUM_TO_FUNCS(signed,char,char)
+DEF_NUM_TO_FUNCS(signed,short,short)
+DEF_NUM_TO_FUNCS(signed,int,int)
+DEF_NUM_TO_FUNCS(signed,long,long)
+DEF_NUM_TO_FUNCS(signed,longlong,long long)
+
+DEF_NUM_TO_FUNCS(unsigned,uchar,unsigned char)
+DEF_NUM_TO_FUNCS(unsigned,ushort,unsigned short)
+DEF_NUM_TO_FUNCS(unsigned,uint,unsigned int)
+DEF_NUM_TO_FUNCS(unsigned,ulong,unsigned long)
+DEF_NUM_TO_FUNCS(unsigned,ulonglong,unsigned long long)
 
 
 template<class Char, class Type, class Rule>
