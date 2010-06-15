9381052765c218163c17de6960ffa2261d9d03fa
victor dunaev (victor dunaev <victor dunaev (victor.dunaev@gmail.com)>
2010-06-15 17:57:10 +1100

    ??????? ?????????? ?? ?????? ??? ???????? wx_Ping

--- whoisalive/wx_Ping.cpp
+++ whoisalive/wx_Ping.cpp
@@ -44,13 +44,21 @@ END_EVENT_TABLE()
 
 #define BLOCK_W 4
 
+void destroy(wx_Ping *frame)
+{
+	frame->Destroy();
+}
+
 wx_Ping::wx_Ping(wxWindow* parent, who::server &server, who::object *object)
-	: terminate_(false)
+	: terminate_(0)
 	, server_(server)
-	, socket_(server.io_service())
 	, object_(object)
+	, pings_socket_(server.io_service())
 	, pings_(1000)
-	, active_index_(-1)
+	, pings_active_index_(-1)
+	, states_socket_(server.io_service())
+	, states_(/*1000*/)
+	, states_active_index_(-1)
 {
 	wxWindowID id = -1;
 
@@ -68,11 +76,11 @@ wx_Ping::wx_Ping(wxWindow* parent, who::server &server, who::object *object)
 	StatePanel = new wxPanel(this, ID_STATEPANEL, wxDefaultPosition, wxSize(400,61), wxTAB_TRAVERSAL, _T("ID_STATEPANEL"));
 	FlexGridSizer1->Add(StatePanel, 1, wxTOP|wxLEFT|wxRIGHT|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
 	BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
-	StateFirstText = new wxStaticText(this, ID_STATICTEXT1, _("\n"), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT, _T("ID_STATICTEXT1"));
+	StateFirstText = new wxStaticText(this, ID_STATICTEXT1, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT, _T("ID_STATICTEXT1"));
 	wxFont StateFirstTextFont(6,wxDEFAULT,wxFONTSTYLE_NORMAL,wxNORMAL,false,wxEmptyString,wxFONTENCODING_DEFAULT);
 	StateFirstText->SetFont(StateFirstTextFont);
 	BoxSizer1->Add(StateFirstText, 1, wxRIGHT|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-	StateLastText = new wxStaticText(this, ID_STATICTEXT2, _("\n"), wxDefaultPosition, wxDefaultSize, wxST_NO_AUTORESIZE|wxALIGN_RIGHT, _T("ID_STATICTEXT2"));
+	StateLastText = new wxStaticText(this, ID_STATICTEXT2, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxST_NO_AUTORESIZE|wxALIGN_RIGHT, _T("ID_STATICTEXT2"));
 	wxFont StateLastTextFont(6,wxDEFAULT,wxFONTSTYLE_NORMAL,wxNORMAL,false,wxEmptyString,wxFONTENCODING_DEFAULT);
 	StateLastText->SetFont(StateLastTextFont);
 	BoxSizer1->Add(StateLastText, 1, wxLEFT|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5);
@@ -80,11 +88,11 @@ wx_Ping::wx_Ping(wxWindow* parent, who::server &server, who::object *object)
 	PingPanel = new wxPanel(this, ID_PINGPANEL, wxDefaultPosition, wxSize(400,61), wxTAB_TRAVERSAL, _T("ID_PINGPANEL"));
 	FlexGridSizer1->Add(PingPanel, 1, wxTOP|wxLEFT|wxRIGHT|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
 	BoxSizer2 = new wxBoxSizer(wxHORIZONTAL);
-	PingFirstText = new wxStaticText(this, ID_STATICTEXT3, _("\n"), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT, _T("ID_STATICTEXT3"));
+	PingFirstText = new wxStaticText(this, ID_STATICTEXT3, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT, _T("ID_STATICTEXT3"));
 	wxFont PingFirstTextFont(6,wxDEFAULT,wxFONTSTYLE_NORMAL,wxNORMAL,false,wxEmptyString,wxFONTENCODING_DEFAULT);
 	PingFirstText->SetFont(PingFirstTextFont);
 	BoxSizer2->Add(PingFirstText, 1, wxRIGHT|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-	PingLastText = new wxStaticText(this, ID_STATICTEXT4, _("\n"), wxDefaultPosition, wxDefaultSize, wxST_NO_AUTORESIZE|wxALIGN_RIGHT, _T("ID_STATICTEXT4"));
+	PingLastText = new wxStaticText(this, ID_STATICTEXT4, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxST_NO_AUTORESIZE|wxALIGN_RIGHT, _T("ID_STATICTEXT4"));
 	wxFont PingLastTextFont(6,wxDEFAULT,wxFONTSTYLE_NORMAL,wxNORMAL,false,wxEmptyString,wxFONTENCODING_DEFAULT);
 	PingLastText->SetFont(PingLastTextFont);
 	BoxSizer2->Add(PingLastText, 1, wxLEFT|wxALIGN_RIGHT|wxALIGN_CENTER_VERTICAL, 5);
@@ -96,10 +104,14 @@ wx_Ping::wx_Ping(wxWindow* parent, who::server &server, who::object *object)
 	SetSizer(FlexGridSizer1);
 	FlexGridSizer1->SetSizeHints(this);
 
-	PingPanel->Connect(ID_PINGPANEL,wxEVT_PAINT,(wxObjectEventFunction)&wx_Ping::on_pingpanel_paint,0,this);
-	PingPanel->Connect(ID_PINGPANEL,wxEVT_ERASE_BACKGROUND,(wxObjectEventFunction)&wx_Ping::on_pingpanel_erasebackground,0,this);
-	PingPanel->Connect(ID_PINGPANEL,wxEVT_MOTION,(wxObjectEventFunction)&wx_Ping::on_pingpanel_mousemove,0,this);
-	PingPanel->Connect(ID_PINGPANEL,wxEVT_LEAVE_WINDOW,(wxObjectEventFunction)&wx_Ping::on_pingpanel_mouseleave,0,this);
+	StatePanel->Connect(ID_STATEPANEL,wxEVT_PAINT,(wxObjectEventFunction)&wx_Ping::OnStatePanelPaint,0,this);
+	StatePanel->Connect(ID_STATEPANEL,wxEVT_ERASE_BACKGROUND,(wxObjectEventFunction)&wx_Ping::OnPanelsEraseBackground,0,this);
+	StatePanel->Connect(ID_STATEPANEL,wxEVT_MOTION,(wxObjectEventFunction)&wx_Ping::OnStatePanelMouseMove,0,this);
+	StatePanel->Connect(ID_STATEPANEL,wxEVT_LEAVE_WINDOW,(wxObjectEventFunction)&wx_Ping::OnStatePanelMouseLeave,0,this);
+	PingPanel->Connect(ID_PINGPANEL,wxEVT_PAINT,(wxObjectEventFunction)&wx_Ping::OnPingPanelPaint,0,this);
+	PingPanel->Connect(ID_PINGPANEL,wxEVT_ERASE_BACKGROUND,(wxObjectEventFunction)&wx_Ping::OnPanelsEraseBackground,0,this);
+	PingPanel->Connect(ID_PINGPANEL,wxEVT_MOTION,(wxObjectEventFunction)&wx_Ping::OnPingPanelMouseMove,0,this);
+	PingPanel->Connect(ID_PINGPANEL,wxEVT_LEAVE_WINDOW,(wxObjectEventFunction)&wx_Ping::OnPingPanelMouseLeave,0,this);
 	Connect(wxID_ANY,wxEVT_CLOSE_WINDOW,(wxObjectEventFunction)&wx_Ping::OnClose);
 	//*)
 
@@ -113,68 +125,278 @@ wx_Ping::wx_Ping(wxWindow* parent, who::server &server, who::object *object)
 	wstring name = object->name() + L" / " + host;
 	SetLabel(name);
 
-	server_.get_header(socket_, reply_,
-		L"/pinger/ping.log?address=" + host);
+	Show();
 
-	reply_.buf_.consume(reply_.buf_.size());
-	reply_.buf_.prepare(65536);
+	//boost::get_deleter<boost::detail::esft2_deleter_wrapper>(
+	//	shared_from_this())->set_deleter(destroy);
+}
+
+void wx_Ping::Start(wxWindow* parent, who::server &server, who::object *object)
+{
+	try
+	{
+		shared_ptr<wx_Ping> ptr( new wx_Ping(parent, server, object) );
+		ptr->start();
+	}
+	catch(my::exception &e)
+	{
+		wxMessageBox(e.message(), L"Ошибка", wxOK | wxICON_ERROR, parent);
+	}
+}
+
+void wx_Ping::start()
+{
+	wstring host = object_->hosts().front();
+
+	/* states */
+	server_.get_header(states_socket_, states_reply_,
+		L"/pinger/state.log?address=" + host);
+	
+	states_reply_.buf_.consume(states_reply_.buf_.size());
+	states_reply_.buf_.prepare(65536);
 
 	asio::async_read_until(
-		socket_, reply_.buf_, "\r\n",
-		boost::bind(&wx_Ping::handle_read, this,
+		states_socket_, states_reply_.buf_, "\r\n",
+		boost::bind(&wx_Ping::states_handle_read, shared_from_this(),
+            boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred) );
+
+	/* pings */
+	server_.get_header(pings_socket_, pings_reply_,
+		L"/pinger/ping.log?address=" + host);
+
+	pings_reply_.buf_.consume(pings_reply_.buf_.size());
+	pings_reply_.buf_.prepare(65536);
+
+	asio::async_read_until(
+		pings_socket_, pings_reply_.buf_, "\r\n",
+		boost::bind(&wx_Ping::pings_handle_read, shared_from_this(),
             boost::asio::placeholders::error,
 			boost::asio::placeholders::bytes_transferred) );
 
 	server_.io_wake_up();
-
-	Show();
 }
 
 wx_Ping::~wx_Ping()
 {
-	terminate_ = true;
-
-	scoped_lock l(read_mutex_);
-	socket_.close();
-
 	//(*Destroy(wx_Ping)
 	//*)
+
+	//if (terminate_ == 2)
+	//	Destroy();
+
+	terminate_ = 2;
+	Close(true);
 }
 
 void wx_Ping::OnClose(wxCloseEvent& event)
 {
-	delete this;
+	terminate_ = 1;
+
+	scoped_lock ls(states_read_mutex_);
+	states_socket_.close();
+
+	scoped_lock lp(pings_read_mutex_);
+	//pings_socket_.close();
+
+	event.Veto();
 }
 
-void wx_Ping::handle_read(const boost::system::error_code& error,
+/* Асинхронное чтение состояний */
+void wx_Ping::states_handle_read(const boost::system::error_code& error,
 	size_t bytes_transferred)
 {
+	scoped_lock l(states_read_mutex_);
+
 	if (terminate_)
 		return;
-
-	scoped_lock l(read_mutex_);
 
 	if (!error)
 	{
 		Refresh();
 
-		reply_.body.resize(bytes_transferred);
-		reply_.buf_.sgetn((char*)reply_.body.c_str(), bytes_transferred);
+		states_reply_.body.resize(bytes_transferred);
+		states_reply_.buf_.sgetn((char*)states_reply_.body.c_str(), bytes_transferred);
 
 		wstringstream out;
 		wxTextAttr style;
 
-		if (reply_.body == "START_ARCHIVE\r\n")
+		if (states_reply_.body == "START_ARCHIVE\r\n")
 		{
 			asio::async_read_until(
-				socket_, reply_.buf_, "END_ARCHIVE\r\n",
-				boost::bind(&wx_Ping::handle_read, this,
+				states_socket_, states_reply_.buf_, "END_ARCHIVE\r\n",
+				boost::bind(&wx_Ping::states_handle_read, shared_from_this(),
 					boost::asio::placeholders::error,
 					boost::asio::placeholders::bytes_transferred) );
 		}
 		else
 		{
-			wistringstream ss( my::utf8::decode(reply_.body) );
+			wistringstream ss( my::utf8::decode(states_reply_.body) );
+
+			/* Если загружаем архив, то это будет больше, чем одна строка */
+			while (true)
+			{
+				/* Первым идёт название хоста */
+				wstring host;
+				ss >> host;
+
+				if (host == L"END_ARCHIVE")
+					continue;
+
+				/* Вторым само состояние */
+				pinger::host_state state;
+				ss >> state;
+
+				if (!ss)
+					break;
+
+				posix_time::ptime utc_time = state.time();
+				state.set_time( my::time::utc_to_local(utc_time) );
+
+				wostringstream out;
+
+				{
+					scoped_lock l(states_mutex_);
+					states_[utc_time] = state;
+					states_active_index_ = -1;
+				}
+			} /* while (ss) */
+
+			states_repaint();
+
+			asio::async_read_until(
+				states_socket_, states_reply_.buf_, "\r\n",
+				boost::bind(&wx_Ping::states_handle_read, shared_from_this(),
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::bytes_transferred) );
+
+		} /* else // if (states_reply_.body == "START_ARCHIVE\r\n") */
+
+		if (first_state_time_.is_special())
+			StateFirstText->SetLabel(L"");
+		else
+			StateFirstText->SetLabel(
+				my::time::to_wstring(first_state_time_, L"%d-%m-%Y\n%H:%M:%S") );
+
+		if (last_state_time_.is_special())
+			StateLastText->SetLabel(L"");
+		else
+			StateLastText->SetLabel(
+				my::time::to_wstring(last_state_time_, L"%d-%m-%Y\n%H:%M:%S") );
+	}
+	else
+	{
+		boost::system::system_error se(error);
+		wstring str = my::str::to_wstring( se.what() );
+
+		wxMessageBox(str, L"Ошибка чтения данных",
+			wxOK | wxICON_ERROR, this);
+	}
+}
+
+/* Прорисовка состояний */
+void wx_Ping::states_repaint()
+{
+	scoped_lock l(states_bitmap_mutex_);
+
+	first_state_time_ = last_state_time_ = posix_time::ptime();
+
+	int w, h;
+	StatePanel->GetClientSize(&w, &h);
+
+	if (states_bitmap_.GetWidth() != w || states_bitmap_.GetHeight() != h)
+		states_bitmap_.Create(w, h);
+
+	wxMemoryDC dc(states_bitmap_);
+
+	int ok_y = h - 4;
+	int warn_y = h /2;
+	int fail_y = 3;
+
+	/* Стираем */
+	dc.SetBrush(*wxBLACK_BRUSH);
+	dc.DrawRectangle(0, 0, w, h);
+
+	/* Рисуем границы */
+	dc.SetPen(*wxGREY_PEN);
+	dc.DrawLine(0, ok_y, w, ok_y);
+	dc.DrawLine(0, warn_y, w, warn_y);
+	dc.DrawLine(0, fail_y, w, fail_y);
+
+	//dc.SetPen(*wxGREEN_PEN);
+
+	int prev_y = -1;
+	int index = 0;
+
+	scoped_lock ll(states_mutex_);
+
+	/*-
+	for (states_list::iterator iter = states_.begin();
+		iter != states_.end(); iter++)
+	{
+		pinger::host_state state = iter->value();
+
+		if (index++ == 0)
+			last_state_time_ = state.time();
+
+		int x = w - index * BLOCK_W;
+
+		if (x <= 0 && x > -BLOCK_W)
+			first_state_time_ = state.time();
+
+		if (state.state() == pinger::ping_result::timeout)
+		{
+			prev_y = -1;
+			continue;
+		}
+
+		int y = zero_y - (double)ping.duration().total_milliseconds()
+			/ timeout * (zero_y - timeout_y);
+		if (y < 0)
+			y = 0;
+
+		if (prev_y != -1)
+			dc.DrawLine(x + BLOCK_W, prev_y, x + BLOCK_W, y);
+
+		dc.DrawLine(x + BLOCK_W, y, x, y);
+
+		prev_y = y;
+	}
+	-*/
+
+	StatePanel->Refresh();
+}
+
+/* Асинхронное чтение пингов */
+void wx_Ping::pings_handle_read(const boost::system::error_code& error,
+	size_t bytes_transferred)
+{
+	scoped_lock l(pings_read_mutex_);
+
+	if (terminate_)
+		return;
+
+	if (!error)
+	{
+		Refresh();
+
+		pings_reply_.body.resize(bytes_transferred);
+		pings_reply_.buf_.sgetn((char*)pings_reply_.body.c_str(), bytes_transferred);
+
+		wstringstream out;
+		wxTextAttr style;
+
+		if (pings_reply_.body == "START_ARCHIVE\r\n")
+		{
+			asio::async_read_until(
+				pings_socket_, pings_reply_.buf_, "END_ARCHIVE\r\n",
+				boost::bind(&wx_Ping::pings_handle_read, shared_from_this(),
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::bytes_transferred) );
+		}
+		else
+		{
+			wistringstream ss( my::utf8::decode(pings_reply_.body) );
 
 			PingTextCtrl->Freeze();
 
@@ -213,7 +435,7 @@ void wx_Ping::handle_read(const boost::system::error_code& error,
 					{
 						scoped_lock l(pings_mutex_);
 						pings_[num] = ping;
-						active_index_ = -1;
+						pings_active_index_ = -1;
 					}
 
 					switch (ping.state())
@@ -246,15 +468,27 @@ void wx_Ping::handle_read(const boost::system::error_code& error,
 
 			PingTextCtrl->Thaw();
 
-			repaint();
+			pings_repaint();
 
 			asio::async_read_until(
-				socket_, reply_.buf_, "\r\n",
-				boost::bind(&wx_Ping::handle_read, this,
+				pings_socket_, pings_reply_.buf_, "\r\n",
+				boost::bind(&wx_Ping::pings_handle_read, shared_from_this(),
 					boost::asio::placeholders::error,
 					boost::asio::placeholders::bytes_transferred) );
 
-		} /* else // if (reply_.body == "START_ARCHIVE\r\n") */
+		} /* else // if (pings_reply_.body == "START_ARCHIVE\r\n") */
+
+		if (first_ping_time_.is_special())
+			PingFirstText->SetLabel(L"");
+		else
+			PingFirstText->SetLabel(
+				my::time::to_wstring(first_ping_time_, L"%d-%m-%Y\n%H:%M:%S") );
+
+		if (last_ping_time_.is_special())
+			PingLastText->SetLabel(L"");
+		else
+			PingLastText->SetLabel(
+				my::time::to_wstring(last_ping_time_, L"%d-%m-%Y\n%H:%M:%S") );
 	}
 	else
 	{
@@ -263,38 +497,22 @@ void wx_Ping::handle_read(const boost::system::error_code& error,
 
 		wxMessageBox(str, L"Ошибка чтения данных",
 			wxOK | wxICON_ERROR, this);
-		Close();
 	}
-
-	StateFirstText->SetLabel(L"[");
-	StateLastText->SetLabel(L"]");
-
-	if (first_ping_.is_special())
-		PingFirstText->SetLabel(L"");
-	else
-		PingFirstText->SetLabel(
-			my::time::to_wstring(first_ping_, L"%d-%m-%Y\n%H:%M:%S") );
-
-	if (last_ping_.is_special())
-		PingLastText->SetLabel(L"");
-	else
-		PingLastText->SetLabel(
-			my::time::to_wstring(last_ping_, L"%d-%m-%Y\n%H:%M:%S") );
 }
 
-void wx_Ping::repaint()
+void wx_Ping::pings_repaint()
 {
-	scoped_lock l(bitmap_mutex_);
+	scoped_lock l(pings_bitmap_mutex_);
 
-	first_ping_ = last_ping_ = posix_time::ptime();
+	first_ping_time_ = last_ping_time_ = posix_time::ptime();
 
 	int w, h;
 	PingPanel->GetClientSize(&w, &h);
 
-	if (bitmap_.GetWidth() != w || bitmap_.GetHeight() != h)
-		bitmap_.Create(w, h);
+	if (pings_bitmap_.GetWidth() != w || pings_bitmap_.GetHeight() != h)
+		pings_bitmap_.Create(w, h);
 
-	wxMemoryDC dc(bitmap_);
+	wxMemoryDC dc(pings_bitmap_);
 
 	int zero_y = h - 4;
 
@@ -323,12 +541,12 @@ void wx_Ping::repaint()
 		pinger::ping_result ping = iter->value();
 
 		if (index++ == 0)
-			last_ping_ = ping.time();
+			last_ping_time_ = ping.time();
 
 		int x = w - index * BLOCK_W;
 
 		if (x <= 0 && x > -BLOCK_W)
-			first_ping_ = ping.time();
+			first_ping_time_ = ping.time();
 
 		if (ping.state() == pinger::ping_result::timeout)
 		{
@@ -352,46 +570,17 @@ void wx_Ping::repaint()
 	PingPanel->Refresh();
 }
 
-void wx_Ping::on_pingpanel_paint(wxPaintEvent& event)
-{
-	scoped_lock l(bitmap_mutex_);
-
-	if (bitmap_.IsOk())
-	{
-		wxPaintDC dc(PingPanel);
-		dc.DrawBitmap(bitmap_, 0, 0);
-
-		if (active_index_ >= 0)
-		{
-			int w, h;
-			PingPanel->GetClientSize(&w, &h);
-
-			dc.SetPen(*wxWHITE_PEN);
-			int x = w - active_index_ * BLOCK_W - BLOCK_W / 2;
-
-			dc.DrawLine(x, 0, x, h);
-		}
-	}
-
-	event.Skip(false);
-}
-
-void wx_Ping::on_pingpanel_erasebackground(wxEraseEvent& event)
-{
-	event.Skip(false);
-}
-
 void wx_Ping::on_pingpanel_mousemove(wxMouseEvent& event)
 {
 	int w, h;
 	PingPanel->GetClientSize(&w, &h);
 
-	active_index_ = (w - event.GetX() + 1) / BLOCK_W;
+	pings_active_index_ = (w - event.GetX() + 1) / BLOCK_W;
 	PingPanel->Refresh();
 
 	scoped_lock l(pings_mutex_);
 
-	int index = active_index_;
+	int index = pings_active_index_;
 	pings_list::iterator iter = pings_.begin();
 
 	while (iter != pings_.end() && index-- != 0)
@@ -412,6 +601,141 @@ void wx_Ping::on_pingpanel_mousemove(wxMouseEvent& event)
 
 void wx_Ping::on_pingpanel_mouseleave(wxMouseEvent& event)
 {
-	active_index_ = -1;
+}
+
+void wx_Ping::OnStatePanelPaint(wxPaintEvent& event)
+{
+	scoped_lock l(states_bitmap_mutex_);
+
+	if (states_bitmap_.IsOk())
+	{
+		wxPaintDC dc(StatePanel);
+		dc.DrawBitmap(states_bitmap_, 0, 0);
+
+		/*-
+		if (states_active_index_ >= 0)
+		{
+			int w, h;
+			StatePanel->GetClientSize(&w, &h);
+
+			dc.SetPen(*wxWHITE_PEN);
+			int x = w - states_active_index_ * BLOCK_W - BLOCK_W / 2;
+
+			dc.DrawLine(x, 0, x, h);
+		}
+		-*/
+	}
+
+	event.Skip(false);
+}
+
+void wx_Ping::OnPingPanelPaint(wxPaintEvent& event)
+{
+	scoped_lock l(pings_bitmap_mutex_);
+
+	if (pings_bitmap_.IsOk())
+	{
+		wxPaintDC dc(PingPanel);
+		dc.DrawBitmap(pings_bitmap_, 0, 0);
+
+		if (pings_active_index_ >= 0)
+		{
+			int w, h;
+			PingPanel->GetClientSize(&w, &h);
+
+			dc.SetPen(*wxWHITE_PEN);
+			int x = w - pings_active_index_ * BLOCK_W - BLOCK_W / 2;
+
+			dc.DrawLine(x, 0, x, h);
+		}
+	}
+
+	event.Skip(false);
+}
+
+void wx_Ping::OnStatePanelEraseBackground(wxEraseEvent& event)
+{
+	event.Skip(false);
+}
+
+void wx_Ping::OnPingPanelEraseBackground(wxEraseEvent& event)
+{
+	event.Skip(false);
+}
+
+void wx_Ping::OnPanelsEraseBackground(wxEraseEvent& event)
+{
+	event.Skip(false);
+}
+
+void wx_Ping::OnStatePanelMouseMove(wxMouseEvent& event)
+{
+	/*-
+	int w, h;
+	StatePingPanel->GetClientSize(&w, &h);
+
+	states_active_index_ = (w - event.GetX() + 1) / BLOCK_W;
+	StatePanel->Refresh();
+
+	scoped_lock l(states_mutex_);
+
+	int index = states_active_index_;
+	states_list::iterator iter = states_.begin();
+
+	while (iter != states_.end() && index-- != 0)
+		iter++;
+
+	if (iter != states_.end())
+	{
+		wostringstream out;
+
+		out << iter->value().state() << endl
+			<< my::time::to_wstring(iter->value().time(), L"%Y-%m-%d %H:%M:%S") << endl
+			<< L"icmp_seq=" << iter->key() << endl
+			<< L"time=" << iter->value().duration().total_milliseconds() << L" ms";
+
+		StatePanel->SetToolTip(out.str());
+	}
+	-*/
+}
+
+void wx_Ping::OnPingPanelMouseMove(wxMouseEvent& event)
+{
+	int w, h;
+	PingPanel->GetClientSize(&w, &h);
+
+	pings_active_index_ = (w - event.GetX() + 1) / BLOCK_W;
+	PingPanel->Refresh();
+
+	scoped_lock l(pings_mutex_);
+
+	int index = pings_active_index_;
+	pings_list::iterator iter = pings_.begin();
+
+	while (iter != pings_.end() && index-- != 0)
+		iter++;
+
+	if (iter != pings_.end())
+	{
+		wostringstream out;
+
+		out << iter->value().state() << endl
+			<< my::time::to_wstring(iter->value().time(), L"%Y-%m-%d %H:%M:%S") << endl
+			<< L"icmp_seq=" << iter->key() << endl
+			<< L"time=" << iter->value().duration().total_milliseconds() << L" ms";
+
+		PingPanel->SetToolTip(out.str());
+	}
+}
+
+void wx_Ping::OnStatePanelMouseLeave(wxMouseEvent& event)
+{
+	states_active_index_ = -1;
+	StatePanel->Refresh();
+}
+
+void wx_Ping::OnPingPanelMouseLeave(wxMouseEvent& event)
+{
+	pings_active_index_ = -1;
 	PingPanel->Refresh();
 }
--- whoisalive/wx_Ping.h
+++ whoisalive/wx_Ping.h
@@ -13,6 +13,7 @@
 #include <memory>
 
 #include <boost/config/warning_disable.hpp> /* против unsafe */
+#include <boost/smart_ptr/enable_shared_from_this2.hpp>
 
 //(*Headers(wx_Ping)
 #include <wx/frame.h>
@@ -23,40 +24,64 @@ class wxFlexGridSizer;
 class wxBoxSizer;
 //*)
 
-#include <wx/textctrl.h> 
-#include <wx/bitmap.h> 
+#include <wx/textctrl.h>
+#include <wx/bitmap.h>
 
-class wx_Ping: public wxFrame
+class wx_Ping: public wxFrame, public boost::enable_shared_from_this2<wx_Ping>
 {
 private:
 	typedef my::mru::list<unsigned short, pinger::ping_result> pings_list;
+	typedef std::map<posix_time::ptime, pinger::host_state> states_list;
 
-	bool terminate_;
+	int terminate_;
 	who::server &server_;
-	tcp::socket socket_;
 	who::object *object_;
 
-	my::http::reply reply_;
-	mutex read_mutex_;
 	pings_list pings_;
-	posix_time::ptime first_ping_;
-	posix_time::ptime last_ping_;
+	tcp::socket pings_socket_;
+	my::http::reply pings_reply_;
+	mutex pings_read_mutex_;
+	posix_time::ptime first_ping_time_;
+	posix_time::ptime last_ping_time_;
 	mutex pings_mutex_;
-	wxBitmap bitmap_;
-	mutex bitmap_mutex_;
-	int active_index_;
+	wxBitmap pings_bitmap_;
+	mutex pings_bitmap_mutex_;
+	int pings_active_index_;
 
-	void handle_read(const boost::system::error_code& error,
+	states_list states_;
+	tcp::socket states_socket_;
+	my::http::reply states_reply_;
+	mutex states_read_mutex_;
+	posix_time::ptime first_state_time_;
+	posix_time::ptime last_state_time_;
+	mutex states_mutex_;
+	wxBitmap states_bitmap_;
+	mutex states_bitmap_mutex_;
+	int states_active_index_;
+
+	void states_handle_read(const boost::system::error_code& error,
 		size_t bytes_transferred);
 
-	void repaint();
+	void states_repaint();
+
+	void pings_handle_read(const boost::system::error_code& error,
+		size_t bytes_transferred);
+
+	void pings_repaint();
 
 	//(*Handlers(wx_Ping)
 	void OnClose(wxCloseEvent& event);
-	void on_pingpanel_paint(wxPaintEvent& event);
 	void on_pingpanel_mousemove(wxMouseEvent& event);
-	void on_pingpanel_erasebackground(wxEraseEvent& event);
 	void on_pingpanel_mouseleave(wxMouseEvent& event);
+	void OnStatePanelPaint(wxPaintEvent& event);
+	void OnStatePanelEraseBackground(wxEraseEvent& event);
+	void OnPingPanelPaint(wxPaintEvent& event);
+	void OnPingPanelEraseBackground(wxEraseEvent& event);
+	void OnPanelsEraseBackground(wxEraseEvent& event);
+	void OnPingPanelMouseMove(wxMouseEvent& event);
+	void OnPingPanelMouseLeave(wxMouseEvent& event);
+	void OnStatePanelMouseMove(wxMouseEvent& event);
+	void OnStatePanelMouseLeave(wxMouseEvent& event);
 	//*)
 
 	DECLARE_EVENT_TABLE()
@@ -72,10 +97,14 @@ protected:
 	static const long ID_STATICTEXT4;
 	static const long ID_PINGTEXTCTRL;
 	//*)
-	
+
+	void start();
+
 public:
 	wx_Ping(wxWindow* parent, who::server &server, who::object *object);
 	virtual ~wx_Ping();
+
+	static void Start(wxWindow* parent, who::server &server, who::object *object);
 
 	//(*Declarations(wx_Ping)
 	wxPanel* StatePanel;
